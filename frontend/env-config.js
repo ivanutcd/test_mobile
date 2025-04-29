75aaaa7267ee7d19de53d151372f5f7672edbceb
Ivan Cruz <kevin.cruz@eneeutcd.hn>
2025-04-29 16:04:08 -0600

    chore: actualizar example variables de entorno

--- frontend/env-config.js
+++ frontend/env-config.js
@@ -1,11 +1,11 @@
 window._env_ = {
-    APP_URL: 'http://localhost:8755',
-    BASE_URL_API: 'http://localhost:5090',
-    BASE_URL_CATALOGO: 'https://dev-api-parametros.eneeutcd.hn/parametros',
-    BASE_URL_SSO: 'https://dev-sso.eneeutcd.hn',
-    VERSION: '1.0.0',
-    SYSTEM_NAME: 'UTCD Cobro Prejuridico',
-    CLIENT_ID: 'utcd_cobro_prejuridico',
-    CLIENT_SCOPE: 'openid roles email server_scope api_scope',
-    IDLE_TIMEOUT: 60,
-  };
+  APP_URL: 'http://localhost:8755',
+  BASE_URL_API: 'http://localhost:5090',
+  BASE_URL_CATALOGO: 'https://dev-api-parametros.eneeutcd.hn/parametros',
+  BASE_URL_SSO: 'https://dev-sso.eneeutcd.hn',
+  VERSION: '1.0.0',
+  SYSTEM_NAME: 'UTCD Cobro Prejuridico',
+  CLIENT_ID: 'cliente_template_enee',
+  CLIENT_SCOPE: 'openid roles email server_scope api_scope',
+  IDLE_TIMEOUT: 60,
+};
