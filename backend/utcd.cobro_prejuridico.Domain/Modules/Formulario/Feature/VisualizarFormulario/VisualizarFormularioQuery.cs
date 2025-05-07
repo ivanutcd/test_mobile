10e29d38dadeab3ba3a4235d23a8214748f4ae40
Jared Salgado <jared.salgado@eneeutcd.hn>
2025-05-07 15:01:09 -0600

    feat: visualizar datos formulario backend

---
+++ backend/utcd.cobro_prejuridico.Domain/Modules/Formulario/Feature/VisualizarFormulario/VisualizarFormularioQuery.cs
@@ -0,0 +1,9 @@
+﻿using Enee.Core.CQRS.Query;
+
+namespace utcd.cobro_prejuridico.Domain.Modules.Formulario.Feature.VisualizarFormulario;
+
+public class VisualizarFormularioQuery : IQuery<Projections.FormularioTable.Formulario>
+{
+    public string Description => "Visualizar un formulario";
+    public Guid idFormulario { get; set; }
+}
