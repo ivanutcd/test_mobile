a3f7adaa4edd778a37a78e475769898101071a06
Ivan Cruz <kevin.cruz@eneeutcd.hn>
2025-06-24 14:32:37 -0600

    feat: endpoint de log

---
+++ backend/utcd.cobro_prejuridico.Api/Modules/Logs/Endpoint.cs
@@ -0,0 +1,18 @@
+﻿using Carter;
+using Enee.IoC.Architecture.Endpoints.TrackingEvent;
+
+namespace utcd.cobro_prejuridico.Api.Modules.Logs;
+
+public class Endpoints : CarterModule
+{
+    public Endpoints()
+        : base("/api/v1/")
+    {
+        this.WithTags("Logs");
+    }
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.EventLogQueryPaginated();
+    }
+}
--- backend/utcd.cobro_prejuridico.Domain/Modules/Formulario/LogDeEventos/Projectors/BitacoraFormulario.cs
+++ backend/utcd.cobro_prejuridico.Domain/Modules/Formulario/LogDeEventos/Projectors/BitacoraFormulario.cs
@@ -1,15 +1,22 @@
 using Enee.Core.CQRS.Query;
 using utcd.cobro_prejuridico.Domain.Modules.Formulario.Feature.CrearFormulario;
+using utcd.cobro_prejuridico.Domain.Modules.Formulario.Feature.EditarFromulario;
+using utcd.cobro_prejuridico.Domain.Modules.Formulario.Feature.EliminarFormulario;
 
 namespace TestApp.Domain.LogDeEventos.Projectors;
 
 public class BitacoraFormulario : EventLogProjector
 {
-    public BitacoraFormulario() : base(logType: "formulario")
+    public BitacoraFormulario()
+        : base(logType: "formulario")
     {
-        
         TrackEvent<FormularioCreadoEvent>()
-            .WithCustomDescription(creado => "Juan");
+            .WithCustomDescription(creado => "Creacion de formulario");
 
+        TrackEvent<FormularioEditarEvent>()
+            .WithCustomDescription(editado => "Edicion de formulario");
+
+        TrackEvent<FormularioEliminado>()
+            .WithCustomDescription(eliminado => "Formulario eliminado");
     }
 }
