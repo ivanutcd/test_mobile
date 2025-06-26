7914737a60d52791156cb90991c4eb6fc44d7b06
Jared Salgado <jared.salgado@eneeutcd.hn>
2025-06-26 15:58:06 -0600

    feat: avance de captura de datos y bugfix de version al editar formularios

--- backend/utcd.cobro_prejuridico.Domain/Modules/Formulario/Projections/FormularioTable/FormularioProjectorcs.cs
+++ backend/utcd.cobro_prejuridico.Domain/Modules/Formulario/Projections/FormularioTable/FormularioProjectorcs.cs
@@ -35,7 +35,7 @@ namespace utcd.cobro_prejuridico.Domain.Modules.Formulario.Projections.Formulari
                     table.Descripcion = evento.Descripcion;
                     table.MovilidadAsociada = evento.MovilidadAsociada;
                     table.Estado = evento.Estado;
-                    table.VersionFormulario = string.Empty;
+                    table.VersionFormulario = evento.VersionFormulario;
                     table.EsEditable = evento.EsEditado;
                 }
             );
